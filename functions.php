2ea8fa0ee74454ebb44eec94923572c023f1ac85
IamM4S <aslam.malimiar@outlook.com>
2024-09-17 16:43:59 +0200

    Initial scripts added

--- functions.php
+++ functions.php
@@ -1,7 +1,22 @@
 <?php
-
+/*
 // Add support for featured images
 add_theme_support( 'post-thumbnails' );
 
 // Automatically add site title to the site header
 add_theme_support( 'title-tag' );
+*/
+
+function nm_theme_assets() {
+
+    //Declare the JS
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true);
+
+    //Declare the style.css file at the root of the theme
+    wp_enqueue_style('style', get_stylesheet_uri(), array(), '1.0');
+
+    //Declare the CSS file in another location
+    wp_enqueue_style('css', get_stylesheet_uri() . '/css/main.css', array(), '1.0');
+
+}
+add_action('wp_enqueue_scripts' , 'nm_theme_assets');
